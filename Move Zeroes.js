e71a139cd69ac893571754bfbc7c3e85021e79f8
bgdnvk <bnovykov@gmail.com>
2021-11-29 06:25:27 +0100

    move zeroes

--- Move Zeroes.js
+++ Move Zeroes.js
@@ -1,4 +1,7 @@
 //https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3157/
+//https://leetcode.com/problems/move-zeroes/
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+// Note that you must do this in-place without making a copy of the array.
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
