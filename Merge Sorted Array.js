0aa795d2836a8d5843160b5a345ccf686878479e
bgdnvk <bnovykov@gmail.com>
2021-11-29 05:53:36 +0100

    merge sorted arrays

--- Merge Sorted Array.js
+++ Merge Sorted Array.js
@@ -1,6 +1,8 @@
+//https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3253/
 // You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 /**
- * //https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3253/
  * @param {number[]} nums1
  * @param {number} m
  * @param {number[]} nums2
@@ -9,14 +11,15 @@
  */
  var merge = function(nums1, m, nums2, n) {
     // console.log(nums1.slice(0,m))
-    
     for (let i = 0; i < n; i++){
         console.log(nums1[m])
+        //since we know that from 'm' the array will be free we can start inserting elements there
         nums1[m] = nums2[i]
         console.log(nums1[m])
+        //incremenet the index 'm' so we can insert the next element properly
         m++
-        
     }
+    //remember to sort the array in a non-decreasing order
     nums1.sort((a,b) => a - b);
     console.log(nums1)
 };
