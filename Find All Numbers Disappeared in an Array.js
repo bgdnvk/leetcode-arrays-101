c7e4ec01df147a416b58005c0c1d7a059877b6af
bgdnvk <bnovykov@gmail.com>
2021-11-29 06:30:30 +0100

    find all nums dis

--- Find All Numbers Disappeared in an Array.js
+++ Find All Numbers Disappeared in an Array.js
@@ -1,4 +1,6 @@
 //https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3270/
+//https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
 /**
  * @param {number[]} nums
  * @return {number[]}
